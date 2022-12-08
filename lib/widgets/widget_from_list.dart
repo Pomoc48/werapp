5c27bc7e30542955322a3da90d0745c05858c891
Pomoc48 <lukawski.mikolaj@gmail.com>
2022-12-08 16:36:18 +0100

    Move widget file

---
+++ lib/widgets/widget_from_list.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:wera_f2/settings.dart';
+
+class WidgetFromList extends StatelessWidget {
+  const WidgetFromList({
+    super.key,
+    required this.children,
+    this.twoColumns = false,
+  });
+
+  final List<Widget> children;
+  final bool twoColumns;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> widgets = [];
+
+    SizedBox columnWidth(List<Widget> widgetList) {
+      return SizedBox(
+        width: 300,
+        child: Column(children: widgetList),
+      );
+    }
+
+    if (twoColumns) {
+      List<Widget> wL = [];
+      List<Widget> wR = [];
+
+      for (int a = 0; a < children.length; a++) {
+        if (a % 2 == 0) {
+          wL.add(children[a]);
+          if (a != children.length - 1) {
+            wL.add(SizedBox(height: Settings.pagePadding * 1.2));
+          }
+        } else {
+          wR.add(children[a]);
+          if (a != children.length - 1) {
+            wR.add(SizedBox(height: Settings.pagePadding * 1.2));
+          }
+        }
+      }
+
+      if (wL.last is SizedBox) wL.removeLast();
+
+      widgets.add(
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(child: columnWidth(wL)),
+            SizedBox(width: Settings.pagePadding),
+            Expanded(child: columnWidth(wR)),
+          ],
+        ),
+      );
+    }
+
+    else {
+      for (int a = 0; a < children.length; a++) {
+        widgets.add(children[a]);
+        if (a != children.length - 1) {
+          widgets.add(SizedBox(height: Settings.pagePadding / 2));
+        }
+      }
+    }
+
+    
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: widgets,
+    );
+  }
+}
